d723d662705d4686b5b4519cf7a0c1059fadd031
rafaa1999 <raferhouma10@gmail.com>
2024-08-14 13:46:11 +0100

    Detect amount input change

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -41,4 +41,9 @@ export class CartComponent implements OnInit {
     this.getCartTotal();
     localStorage.setItem('cart', JSON.stringify(this.cartProducts));
   }
+
+  detectChange() {
+    this.getCartTotal()
+    localStorage.setItem("cart" , JSON.stringify(this.cartProducts))
+  }
 }
