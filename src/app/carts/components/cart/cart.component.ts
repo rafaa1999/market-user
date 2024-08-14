6f6d6fc2b02dc8e41994d740a958e7d4964ff474
rafaa1999 <raferhouma10@gmail.com>
2024-08-14 12:26:23 +0100

    Get all added products from the local storage and display then in the cart

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartProducts: any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getCartProducts();
+  }
+
+  getCartProducts() {
+    if ('cart' in localStorage) {
+      this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
+    }
+  }
 }
